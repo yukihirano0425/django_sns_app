a7ea37bd1effad99b3c652c3b9ac284c1a74f6a1
yukihirano0425 <yuki.hirano@uzabase.com>
2023-06-22 22:08:05 +0900

    初期設定の追加

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # django_sns_app
+
+## activate python venv
+source .venv/bin/activate
+
+## deactivate python venv
+deactivate
+
+# install python library
+pip install -r requirements.txt
+
+# start app
+python manage.py runserver
--- snsapp/models.py
+++ snsapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __init__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_at"]
